0c4aa443a9eee21a4ef33b1ecd578e72d31a67fe
Ethan <ethan.park@sodacrew.com>
2022-07-14 17:18:29 +0900

    Add ios config

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,8 +1,14 @@
+import { withIosKakaoLogin } from "./ios/withIosKakaoLogin";
 import type { ConfigPlugin } from "@expo/config-plugins";
 
-module.exports = function withPlugins(
-  config: ConfigPlugin,
-  props: any
-): ConfigPlugin {
+export interface KakaoLoginPluginProps {
+  kakaoAppKey: string;
+  overriderKakaoSDKVersion?: string;
+}
+
+const withPlugins: ConfigPlugin<KakaoLoginPluginProps> = (config, props) => {
+  config = withIosKakaoLogin(config, props);
   return config;
 };
+
+export default withPlugins;
---
+++ plugins/ios/withIosKakaoLogin.ts
@@ -0,0 +1,91 @@
+import {
+  ConfigPlugin,
+  WarningAggregator,
+  withAppDelegate,
+  withInfoPlist,
+} from "@expo/config-plugins";
+import { KakaoLoginPluginProps } from "..";
+
+const KAKAO_SCHEMES = ["kakaokompassauth", "storykompassauth", "kakaolink"];
+
+const KAKAO_HEADER_IMPORT_STRING = "#import <RNKakaoLogins.h>";
+
+const KAKAO_LINKING_STRING = `if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
+  return [RNKakaoLogins handleOpenUrl: url];
+}`;
+const modifyInfoPlist: ConfigPlugin<KakaoLoginPluginProps> = (
+  config,
+  props
+) => {
+  return withInfoPlist(config, async (config) => {
+    if (!config.modResults.KAKAO_APP_KEY) {
+      config.modResults.KAKAO_APP_KEY = props.kakaoAppKey;
+    }
+
+    const NEW_URL_TYPES = `kakao${props.kakaoAppKey}`;
+
+    if (!Array.isArray(config.modResults.CFBundleURLTypes)) {
+      config.modResults.CFBundleURLTypes = [];
+    }
+
+    const urlType = config.modResults.CFBundleURLTypes.find((item) =>
+      item.CFBundleURLSchemes.includes(NEW_URL_TYPES)
+    );
+
+    if (!urlType) {
+      config.modResults.CFBundleURLTypes.push({
+        CFBundleURLSchemes: [NEW_URL_TYPES],
+      });
+    }
+
+    if (!Array.isArray(config.modResults.LSApplicationQueriesSchemes)) {
+      config.modResults.LSApplicationQueriesSchemes = KAKAO_SCHEMES;
+    } else {
+      KAKAO_SCHEMES.forEach(
+        (scheme) =>
+          !config.modResults.LSApplicationQueriesSchemes?.includes(scheme) &&
+          config.modResults.LSApplicationQueriesSchemes?.push(scheme)
+      );
+    }
+
+    return config;
+  });
+};
+
+const modifyAppDelegate: ConfigPlugin = (config) => {
+  const modifyContents = (contents: string) => {
+    if (!contents.includes(KAKAO_HEADER_IMPORT_STRING)) {
+      contents = `${KAKAO_HEADER_IMPORT_STRING}\n`.concat(contents);
+    }
+
+    if (!contents.includes(KAKAO_LINKING_STRING))
+      contents = contents.replace(
+        "options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {",
+        `options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+      ${KAKAO_LINKING_STRING}`
+      );
+    return contents;
+  };
+
+  return withAppDelegate(config, (config) => {
+    if (["objc", "objcpp"].includes(config.modResults.language)) {
+      config.modResults.contents = modifyContents(config.modResults.contents);
+    } else {
+      WarningAggregator.addWarningIOS(
+        "withFacebook",
+        "Swift AppDelegate files are not supported yet."
+      );
+    }
+    return config;
+  });
+};
+
+export const withIosKakaoLogin: ConfigPlugin<KakaoLoginPluginProps> = (
+  config,
+  props
+) => {
+  config = modifyInfoPlist(config, props);
+  config = modifyAppDelegate(config);
+
+  return config;
+};
