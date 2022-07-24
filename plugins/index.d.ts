52625b159dbb9f5d20cf6dc22bf9d3ee1a7d497d
Ethan <ethan.park@sodacrew.com>
2022-07-24 23:39:16 +0900

    v1.2.0

--- plugins/index.d.ts
+++ plugins/index.d.ts
@@ -1,1 +1,7 @@
-export {};
+import type { ConfigPlugin } from "@expo/config-plugins";
+export interface KakaoLoginPluginProps {
+    kakaoAppKey: string;
+    overrideKakaoSDKVersion?: string;
+}
+declare const withPlugins: ConfigPlugin<KakaoLoginPluginProps>;
+export default withPlugins;
--- plugins/index.js
+++ plugins/index.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = function withPlugins(config, props) {
+const withIosKakaoLogin_1 = require("./ios/withIosKakaoLogin");
+const withPlugins = (config, props) => {
+    config = withIosKakaoLogin_1.withIosKakaoLogin(config, props);
     return config;
 };
+exports.default = withPlugins;
